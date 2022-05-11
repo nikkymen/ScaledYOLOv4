b5c1322e0ba0c4472d2c7a882ad3c336e7a99cb0
nikita.kozhanov <nikita.kozhanov@kronshtadt.ru>
2022-05-11 18:33:24 +0300

    export

--- models/yolo.py
+++ models/yolo.py
@@ -33,7 +33,11 @@ class Detect(nn.Module):
         z = []  # inference output
         self.training |= self.export
         for i in range(self.nl):
-            x[i] = self.m[i](x[i])  # conv
+            x[i] = self.m[i](x[i])  # output conv
+
+            if self.export:
+                continue
+
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
